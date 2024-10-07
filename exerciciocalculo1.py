b12dd6e30ac87a7b4d4e62eeab6def050263c165
Victor Vianna <silvavvictor07@gmail.com>
2024-10-06 23:13:40 -0300

    Create exerciciocalculo1.py
    Operações básicas com Python

---
+++ exerciciocalculo1.py
@@ -0,0 +1,7 @@
+print("Olá, este é um programa para converter temperatura de Celsius para Fahrenheit")
+C=input("informe a temperatura em graus Celsius: ")
+iC=eval(C)
+F=1.8 *iC + 32
+int_F=int(F)
+print('A temperatura convertida em Farenheit:' ,int_F)
+print('Muito obrigado! Volte sempre!')
